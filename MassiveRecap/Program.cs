ffcc4293a092903f2748df68f16354c033329e18
olga-vorontsovskaya <o.vorontsovskaya@gmail.com>
2024-04-27 16:36:36 +0300

    Add project files.

---
+++ MassiveRecap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[3];
+
+            
+            for (int i = 0; i < arr1.Length; i++) //присваивание значения массиву
+            {
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("-------------------------"); // разделитель ввода и вывода
+            for (int i = 0; i < arr1.Length; i++) //вывод значения
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
